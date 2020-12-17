07c7bccc8fb604d145b4ca286d1224630154a83a
Andrey <villejam@yandex.ru>
2020-12-17 23:31:28 +0300

    Final task module3

--- Module3ConsoleApp/Program.cs
+++ Module3ConsoleApp/Program.cs
@@ -6,25 +6,14 @@ namespace Module3ConsoleApp
     {
         static void Main(string[] args)
         {
-            string MyName = "Andrey";
-            byte MyAge = 27;
-            bool HaveIAPet = true;
-            double MyShoesSize = 42.0;
-            string name = "q";
-            var age = Convert.ToInt32(Console.ReadLine());
-            string favcolor = "white";
-            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
-            var olddata = 22;
-            byte data = checked ((byte) int.Parse(Console.ReadLine()));
-
-            Console.WriteLine(data);
-            Console.WriteLine(day);
-            Console.WriteLine($"My name is {MyName}");
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIAPet);
-            Console.WriteLine("My shoes size is " + MyShoesSize);
-            Console.WriteLine("\tПривет,\n мир!");
-            Console.WriteLine("{0}\n{1}\n{2}", name, age, favcolor);
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = Convert.ToByte(Console.ReadLine());           
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("Enter your birthdate: ");
+            string bdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is: {0}", bdate);
             Console.ReadKey();
         }
     }
