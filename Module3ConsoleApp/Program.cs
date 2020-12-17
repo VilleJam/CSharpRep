e949641494984f9def3922bdaa055abc9d3b9c6c
Andrey <villejam@yandex.ru>
2020-12-17 22:33:07 +0300

    Module 3 before math

--- Module3ConsoleApp/Program.cs
+++ Module3ConsoleApp/Program.cs
@@ -10,12 +10,16 @@ namespace Module3ConsoleApp
             byte MyAge = 27;
             bool HaveIAPet = true;
             double MyShoesSize = 42.0;
+            string name = "q";
+            int age = 2;
+            string favcolor = "white";
 
-            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine($"My name is {MyName}");
             Console.WriteLine("My age is " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIAPet);
             Console.WriteLine("My shoes size is " + MyShoesSize);
-
+            Console.WriteLine("\tПривет,\n мир!");
+            Console.WriteLine("{0}\n{1}\n{2}", name, age, favcolor);
             Console.ReadKey();
         }
     }
