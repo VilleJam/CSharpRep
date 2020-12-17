b82f9aef5dc345b71f970b269897b7e18c6155ed
Andrey <villejam@yandex.ru>
2020-12-17 20:52:51 +0300

    Добавьте файлы проекта.

---
+++ Module3ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module3ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет,\n мир!");
+            Console.WriteLine("\x23");
+        }
+    }
+}
