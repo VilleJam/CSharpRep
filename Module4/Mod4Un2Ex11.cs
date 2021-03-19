9a2dfd00f29573ae1eefe5fa752c69b226eb15d7
VilleJam <villejam@yandex.ru>
2021-03-19 13:50:13 +0300

    Ex 4.2.11

---
+++ Module4/Mod4Un2Ex11.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Module4Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Цикл do-while");
+            int t = 0;
+
+            do
+            {
+                Console.WriteLine(t);
+
+                Console.WriteLine("Write ur fav color");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ur color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ur color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Ur color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Ur color is not in this list!");
+                        break;
+                }
+
+                t++;
+            } while (t < 5);
+        }
+    }
+}
