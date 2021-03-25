fa0f9e6b7f13d7f7e22543aee8a0f35cf49c9b28
VilleJam <villejam@yandex.ru>
2021-03-25 10:05:14 +0300

    massivesmod4

--- Module4/ConsoleApp1/ConsoleApp1/massives.cs
+++ Module4/ConsoleApp1/ConsoleApp1/massives.cs
@@ -6,23 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
-			int posSumm = 0;
+            int posSumm = 0;
 
-			foreach (var item in arr)
-			{
-				
+            foreach (var item in arr)
+            {
+
                 if (item > 0)
-                posSumm++;
-			}
+                    posSumm++;
+            }
 
             Console.WriteLine(posSumm);
 
             for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
             {
                 for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
+                {
+
+                }
 
             }
+        }
     }
 }
