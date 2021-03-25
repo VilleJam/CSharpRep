fde44b7571375245fd1e1a8ed6be192546daf2fc
VilleJam <villejam@yandex.ru>
2021-03-25 10:03:45 +0300

    module4massives

---
+++ Module4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+			int posSumm = 0;
+
+			foreach (var item in arr)
+			{
+				
+                if (item > 0)
+                posSumm++;
+			}
+
+            Console.WriteLine(posSumm);
+
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
+
+            }
+    }
+}
