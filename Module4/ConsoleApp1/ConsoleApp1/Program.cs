aa6f2e72cc505c7a1c8e1097c45e9dd86096a7a4
VilleJam <villejam@yandex.ru>
2021-03-26 21:43:31 +0300

    111

--- Module4/ConsoleApp1/ConsoleApp1/Program.cs
+++ Module4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,23 +6,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
-			int posSumm = 0;
+            int posSumm = 0;
 
-			foreach (var item in arr)
-			{
-				
+            int num;
+
+            foreach (var item in arr)
+            {
+
                 if (item > 0)
-                posSumm++;
-			}
+                    posSumm++;
+            }
 
             Console.WriteLine(posSumm);
 
             for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
             {
                 for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
+                {
+                    for (int j = i + 1; j < arr.GetUpperBound(1) + 1; j++)
+                    {
+                        if (arr[i, k] > arr[i, j])
+                        {
+                            num = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = num;
+                        }
+                    }
+                }
+            }
+
+            for (int q = 0; q < arr.GetUpperBound(0) + 1; q++)
+            {
+                for (int w = 0; w < arr.GetUpperBound(1) + 1; w++)
+                    Console.Write(arr[q, w] + " ");
+
+                Console.WriteLine();
 
             }
+        }
     }
 }
