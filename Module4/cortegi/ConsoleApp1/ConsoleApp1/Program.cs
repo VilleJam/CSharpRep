fb45171c0579e35ae29e752eb0a445b181458757
VilleJam <villejam@yandex.ru>
2021-03-26 22:11:11 +0300

    korteg1

---
+++ Module4/cortegi/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+            Console.WriteLine("Введите ваше имя:");
+            anketa.name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст цифрами:");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+        }
+    }
+}
